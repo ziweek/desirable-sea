c56342a9cf08aaa8cc993f594b067673320976e2
Alex Jiuk Kim <basecamp@Alexs-Mac-mini.local>
2024-01-14 20:38:57 +0900

    fix width criteria

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
     query: "(min-width: 1224px)",
   });
   const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 720px)" });
   const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
   const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
 
